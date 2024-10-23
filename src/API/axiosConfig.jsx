a4c51c9a78cb8801fc4d5ad30b1c6cef19342048
kemaw <kemawseattl@gmail.com>
2024-10-23 11:54:19 -0700

    update baseURL

--- src/API/axiosConfig.jsx
+++ src/API/axiosConfig.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosBase = axios.create({
   // baseURL: "http://localhost:5500/api",
-  baseURL: "https://evangadi-forum-backend-deploy-1-05zz.onrender.com/api",
+  // baseURL: "https://evangadi-forum-backend-deploy-1-05zz.onrender.com/api",
+  baseURL: "https://evangadi-forum-server-wwfl.onrender.com/api",
 });
 
 export default axiosBase;
